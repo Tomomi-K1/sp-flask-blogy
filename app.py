c6aa6f09cd389013f4ebb8ac76dc10ca4a5ef2cc
Tomomi-K1 <tomomi.music@gmail.com>
2022-10-09 17:01:51 -0700

    part two

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 """Blogly application."""
 
 
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
+import datetime
 
 
 app = Flask(__name__)
@@ -16,7 +17,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS']=False
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
-
+db.create_all()
 
 @app.route('/')
 def redirect_to_user_list():
@@ -25,14 +26,15 @@ def redirect_to_user_list():
 @app.route('/users')
 def show_all_users():
     """shows list of all users"""
-    users = User.query.all()
+    users = User.query.order_by(User.last_name, User.first_name).all()
     return render_template('users-list.html', users=users)
 
 @app.route('/users/<int:user_id>')
 def show_user_detail(user_id):
     """Show user's detailed information"""  
     user = User.query.get_or_404(user_id) 
-    return render_template('details.html', user=user)
+    posts = Post.query.filter(Post.user_id == user_id)
+    return render_template('details.html', user=user, posts=posts)
 
 
 @app.route('/users/new')
@@ -44,20 +46,26 @@ def post_new_user():
     """add a new user to users table"""
     first_name = request.form['first_name']
     last_name = request.form['last_name']
-    url = request.form.get('url')
+    url = request.form['url']
+    url = url if url else None
 
     new_user = User(first_name=first_name, last_name=last_name, image_url=url)
+    # springboard solution
+    # new_user = User(
+    #     first_name=request.form['first_name'],
+    #     last_name=request.form['last_name'],
+    #     image_url=request.form['image_url'] or None)
+    
+    
     db.session.add(new_user)
     db.session.commit()
 
     return redirect('/users')   
 
-
-
 @app.route('/users/<int:user_id>/edit')
 def show_edit_page(user_id):
     """show edit form"""    
-    user=User.query.get(user_id)
+    user=User.query.get_or_404(user_id)
     return render_template('edit.html', user=user)
 
 @app.route('/users/<int:user_id>/edit', methods=['POST'])
@@ -65,12 +73,13 @@ def process_edit(user_id):
 
     first_name = request.form['first_name']
     last_name = request.form['last_name']
-    url = request.form.get('url')
+    url = request.form['url']
     
-    user = User.query.get(user_id)
+    user = User.query.get_or_404(user_id)
     user.first_name = first_name
     user.last_name = last_name
-    user.image_url = url
+    # user.image_url = url 
+    user.image_url = url if url else "/static/default-pic.png" 
 
     db.session.add(user)
     db.session.commit()
@@ -81,11 +90,78 @@ def process_edit(user_id):
 def delete_user(user_id):
     """delete a user and redirect to the list of users"""
 
-    user =User.query.get(user_id)
+    user =User.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
 
     return redirect('/users')
+
+# ======================== Posts route============================
+@app.route('/users/<int:user_id>/posts/new')
+def new_post_form(user_id):
+    """Show form to add a post for that user"""
+    user =User.query.get(user_id)
+
+    return render_template('post-form.html', user=user)
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def add_new_post(user_id):
+    """Handle add form; add post and redirect to the user detail page"""
+
+    title = request.form['title']
+    content = request.form['content']
+    
+    new_post = Post(title=title, content=content, user_id=user_id )
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(url_for('show_user_detail', user_id=user_id))
+
+@app.route('/posts/<int:post_id>')
+def show_post_details(post_id):
+    """Show a post.Show buttons to edit and delete the post."""
+    
+    post =Post.query.get(post_id)
+
+    return render_template('post-detail.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["GET"])
+def show_post_edit_form(post_id):
+    """Show a form to edit an existing post"""
+    post = Post.query.get(post_id)
+    return render_template('post-edit-form.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def post_editted_post(post_id):
+    """Handle form submission for updating an existing post"""
+    title = request.form['title']
+    content = request.form['content']
+    
+    post = Post.query.get(post_id)
+    post.title = title
+    post.content = content 
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for('show_post_details', post_id =post_id))
+    # or you could write return redirect(f"/users/{post.user_id}')
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    """delete a user and redirect to the list of users"""
+
+    post =Post.query.get(post_id)
+    user_id=post.user_id
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(url_for('show_user_detail', user_id=user_id))
+     # or you could write return redirect(f"/users/{post.user_id}')
+
+
+
+
 
 
 
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -17,8 +18,27 @@ class User(db.Model):
     last_name = db.Column(db.String(20), nullable = False)
     image_url = db.Column(db.Text, nullable = False, default="/static/default-pic.png")
 
-    def __rep__(self):
+
+    def __repr__(self):
         """Show info about user"""
 
-        return f'<User {self.id}{self.first_name}{self.last_name}{self.image_url}>'
+        return f'<User {self.id} {self.first_name} {self.last_name} {self.image_url}>'
      
+class Post(db.Model):
+    """ create post instances"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    title = db.Column(db.String(50), nullable = False)
+    content = db.Column(db.String(1000), nullable = False)
+    created_at = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='posts')
+
+
+    def __repr__(self):
+        """Show info about user"""
+
+        return f'<post {self.id} {self.title}>'
